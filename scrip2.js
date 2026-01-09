738e3235bf86bfcb6323f076681d3f5e18ec0439
Patrycja <patrycja.pazdzierz@gmail.com>
2026-01-09 19:24:57 +0100

    new numbers

--- scrip2.js
+++ scrip2.js
@@ -1,7 +1,11 @@
 let a = 15
 let b = 3
 
-console.log(a+b)
-console.log(a-b)
-console.log(a*b)
-console.log(a/b)
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b)
+
+console.log(a%b)
+console.log(a++)
+console.log(b--)
