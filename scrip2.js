5fb1298dfae4c2784324fccd840df2b802069314
Patrycja <patrycja.pazdzierz@gmail.com>
2026-01-12 16:22:01 +0100

    added new change

--- scrip2.js
+++ scrip2.js
@@ -1,4 +1,8 @@
-let x = 15
+let x = 115
 if (x==15){
     console.log('Wartosc x = 15')
+}else if (x<15){
+    console.log('Wartosc x jest mniejsza od 15')
+}else if (x>15){
+    console.log('Wartosc x jest wieksza od 15')
 }
