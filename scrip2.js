99f55c78db2900a6728767036c089e0c886454ef
Patrycja <patrycja.pazdzierz@gmail.com>
2026-01-12 16:30:09 +0100

    numbers

--- scrip2.js
+++ scrip2.js
@@ -1,8 +1,13 @@
-let x = 115
-if (x==15){
-    console.log('Wartosc x = 15')
-}else if (x<15){
-    console.log('Wartosc x jest mniejsza od 15')
-}else if (x>15){
-    console.log('Wartosc x jest wieksza od 15')
+// let x = 115
+// if (x==15){
+//     console.log('Wartosc x = 15')
+// }else if (x<15){
+//     console.log('Wartosc x jest mniejsza od 15')
+// }else if (x>15){
+    // console.log('Wartosc x jest wieksza od 15')
+
+if (x % 2 === 0){
+    console.loge('Jest to liczba parzysta')
+}else{
+    console.log('Jest to liczba nieparzysta')
 }
