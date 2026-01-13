4d83a212712663ac04f4dc933db81e8a9e5ac4e7
Patrycja <patrycja.pazdzierz@gmail.com>
2026-01-13 16:08:16 +0100

    new task

--- scrip2.js
+++ scrip2.js
@@ -1,10 +1,14 @@
-let x = 5
+let x = 3
 switch (x) {
     case 1:
-        console.log('x wynosi 1')
-        break;
+    case 2:
     case 3:
-        console.log('x wynosi 3') 
+        if (x==1) console.log('x wynosi 1')
+        else if (x==2) console.log('x wynosi 2')
+        else console.log('x wynosi 3')
+        break;
+    case 4:
+        console.log('x wynosi 4') 
         break; 
     case 5:
         console.log('x wynosi 5')
