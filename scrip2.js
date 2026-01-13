97e6e729a8e51c1e574ba5a30ec715a2d1484a46
Patrycja <patrycja.pazdzierz@gmail.com>
2026-01-13 17:55:27 +0100

    new change

--- scrip2.js
+++ scrip2.js
@@ -1,3 +1,7 @@
-for (let i = 0 ; i<=10 ; i++){
-    console.log('kolejne przejscie petli  '+i)
+// for (let i = 0 ; i<10 ; i++){
+//     console.log('kolejne przejscie petli  '+i)
+// }
+
+for (let i = 2 ; i<=10 ; i++){
+    if (i % 2 === 0) console.log(i)
 }
