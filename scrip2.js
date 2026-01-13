8767e2c155151981d2e02b5ca4e718e0f9fe6d10
Patrycja <patrycja.pazdzierz@gmail.com>
2026-01-13 15:57:40 +0100

    new change added

--- scrip2.js
+++ scrip2.js
@@ -2,10 +2,13 @@ let x = 5
 switch (x) {
     case 1:
         console.log('x wynosi 1')
+        break;
     case 3:
-        console.log('x wynosi 3')  
+        console.log('x wynosi 3') 
+        break; 
     case 5:
         console.log('x wynosi 5')
+        break;
     default:
         console.log('zadna z powyzszych opcji nie zostala wybrana')          
 }
